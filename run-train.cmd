7ad6f139b52ae2640337606fcdb455d9a79bbc8b
sungjoonkim <ted@bitflow.ai>
2025-10-24 22:16:44 +0900

    update

--- run-train.cmd
+++ run-train.cmd
@@ -3,7 +3,7 @@ setx CUDA_VISIBLE_DEVICES "1"
 accelerate launch --num_cpu_threads_per_process 1 --mixed_precision bf16 ^
   sdxl_train_network.py ^
   --pretrained_model_name_or_path="./models/stable-diffusion-xl-base-1.0" ^
-  --train_data_dir="./dataset/mainchar" ^
+  --train_data_dir="./dataset/train/mainchar" ^
   --output_dir="./output_model" ^
   --logging_dir="./logs" ^
   --output_name="karina" ^
