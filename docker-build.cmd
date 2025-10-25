6d162d2f7d4ee673aba3335b06dfae78ed2a591b
sungjoonkim <ted@bitflow.ai>
2025-10-26 03:25:22 +0900

    update

--- docker-build.cmd
+++ docker-build.cmd
@@ -1,3 +1,3 @@
-docker build --no-cache -t aicompanion/sdxl_train_captioner:0.8.7 .
+docker build --no-cache -t aicompanion/sdxl_train_captioner:0.8.8 .
 
-docker tag aicompanion/sdxl_train_captioner:0.8.7 aicompanion/sdxl_train_captioner:latest
+docker tag aicompanion/sdxl_train_captioner:0.8.8 aicompanion/sdxl_train_captioner:latest
